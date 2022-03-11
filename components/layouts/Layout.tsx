3bbf246ccf1da3c266275302e9909fa63798c71b
italojar <jaramilloarmijositalo@gmail.com>
2022-03-11 23:22:56 +0100

    Work in progress Pokemon App

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -22,8 +22,8 @@ export const Layout:FC<Props> = ({children, title}) => {
             <meta property="og:image" content={`${origin}/open-graph/banner.png`} />
             <meta property="og:image:secure_url" content={`${origin}/open-graph/banner.png`} />
             <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="300" />
-            <meta property="og:image:height" content="300" />
+            <meta property="og:image:width" content="200" />
+            <meta property="og:image:height" content="200" />
         </Head>
 
         <NavBar/>
